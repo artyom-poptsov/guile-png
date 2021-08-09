7326c18bba654cb5b53ffba2c6a804d29acaf90a
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2021-08-09 07:16:32 +0300

    modules/png/core/chunk-iend.scm: Add object printers

--- modules/png/core/chunk-iend.scm
+++ modules/png/core/chunk-iend.scm
@@ -1,10 +1,23 @@
 (define-module (png core chunk-iend)
   #:use-module (oop goops)
+  #:use-module (png core common)
   #:use-module (png core chunk)
   #:export (<png-chunk:IEND>))
 
 
 
 (define-class <png-chunk:IEND> (<png-chunk>))
+
+(define-method (%display (chunk <png-chunk:IEND>) (port <port>))
+  (let ((type (vector->chunk-type (png-chunk-type chunk))))
+    (format port "#<png-chunk:IEND ~a ~a>"
+            (list-ref type 2)
+            (object-address/hex-string chunk))))
+
+(define-method (display (chunk <png-chunk:IEND>) (port <port>))
+  (%display chunk port))
+
+(define-method (write (chunk <png-chunk:IEND>) (port <port>))
+  (%display chunk port))
 
 ;;; chunk-iend.scm ends here.
