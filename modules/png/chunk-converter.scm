6e8cf16ec96385b5a8c53c981a568570ee885d7f
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2021-11-13 22:51:30 +0300

    modules/png/chunk-converter.scm: Fix the converter

--- modules/png/chunk-converter.scm
+++ modules/png/chunk-converter.scm
@@ -9,27 +9,43 @@
   #:use-module (png core chunk-time)
   #:use-module (png core chunk-iccp)
   #:use-module (png core chunk-phys)
+  #:use-module (png core chunk-bkgd)
+  #:use-module (png image)
   #:export (png-chunk->typed-chunk))
 
 
-(define %converters-to-typed
-  `((IHDR . ,data->png-chunk:IHDR)
-    (PLTE . ,data->png-chunk:PLTE)
-    (IEND . ,data->png-chunk:IEND)
-    (cHRM . ,data->png-chunk:cHRM)
-    (zTXt . ,data->png-chunk:zTXt)
-    (tIME . ,data->png-chunk:tIME)
-    (iCCP . ,data->png-chunk:iCCP)
-    (pHYs . ,data->png-chunk:pHYs)))
+(define (make-converter proc)
+  (lambda (image chunk)
+    (proc (png-chunk-data   chunk)
+          (png-chunk-type   chunk)
+          (png-chunk-length chunk)
+          (png-chunk-crc    chunk))))
 
-(define-method (png-chunk->typed-chunk (chunk <png-chunk>))
+(define %converters-to-typed
+  `((IHDR . ,(make-converter data->png-chunk:IHDR))
+    (PLTE . ,(make-converter data->png-chunk:PLTE))
+    (IEND . ,(make-converter data->png-chunk:IEND))
+    (cHRM . ,(make-converter data->png-chunk:cHRM))
+    (zTXt . ,(make-converter data->png-chunk:zTXt))
+    (tIME . ,(make-converter data->png-chunk:tIME))
+    (iCCP . ,(make-converter data->png-chunk:iCCP))
+    (pHYs . ,(make-converter data->png-chunk:pHYs))
+    (bKGD . ,(lambda (image chunk)
+               (data->png-chunk:bKGD (png-chunk-data   chunk)
+                                     (png-chunk-type   chunk)
+                                     (png-chunk-length chunk)
+                                     (png-chunk-crc    chunk)
+                                     (png-chunk:IHDR-colour-type
+                                      (car (png-image-chunks-query image 'IHDR))))))))
+
+
+
+(define-method (png-chunk->typed-chunk (image <png-image>)
+                                       (chunk <png-chunk>))
   (let ((type (png-chunk-type/name chunk)))
     (if type
         (let ((converter (assoc-ref %converters-to-typed type)))
           (if converter
-              (converter (png-chunk-data   chunk)
-                         (png-chunk-type   chunk)
-                         (png-chunk-length chunk)
-                         (png-chunk-crc    chunk))
+              (converter image chunk)
               chunk))
         (error "Unknown chunk type" type chunk))))
