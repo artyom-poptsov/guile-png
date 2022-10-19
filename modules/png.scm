c8486ee265f6983c710f86feedbae86adf056c06
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2022-10-19 21:45:06 +0300

    modules/png.scm: Add the module commentary

--- modules/png.scm
+++ modules/png.scm
@@ -1,3 +1,30 @@
+;;; png.scm -- GNU Guile PNG parser.
+
+;; Copyright (C) 2022 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; The program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with the program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; Guile-PNG is PNG format (RFC5545) [1] parser for GNU Guile.
+;;
+;; [1] https://tools.ietf.org/html/rfc2083
+
+
+;;; Code:
+
 (define-module (png)
   #:use-module (oop goops)
   #:use-module (smc core log)
