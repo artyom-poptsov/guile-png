94e13c22688c6bbde6aa7ee85b7b83c034eb754f
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2022-10-23 20:45:05 +0300

    png/core/common (int32->bytevector): New method
    * modules/png/core/common.scm (int32->bytevector): New method.

--- modules/png/core/common.scm
+++ modules/png/core/common.scm
@@ -2,7 +2,8 @@
   #:use-module (oop goops)
   #:export (object-address/hex-string
             vector->int32
-            vector->int16))
+            vector->int16
+            int32->bytevector))
 
 
 (define (object-address/hex-string object)
@@ -18,3 +19,12 @@
 (define-method (vector->int16 (v <vector>))
   (logior (ash (vector-ref v 0) 8)
           (vector-ref v 1)))
+
+(define-method (int32->bytevector (number <number>))
+  "Convert a NUMBER to a byte vector."
+  (u8-list->bytevector
+   (list (ash (logand v #xFF000000) -24)
+         (ash (logand v #x00FF0000) -16)
+         (ash (logand v #x0000FF00) -8)
+         (logand v #x000000FF))))
+
