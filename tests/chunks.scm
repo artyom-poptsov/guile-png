4f24ed8d62d655fb648e55cd4a9db152fc0202da
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2022-10-23 21:22:32 +0300

    tests/chunks.scm: Update

--- tests/chunks.scm
+++ tests/chunks.scm
@@ -11,51 +11,51 @@
 (test-begin %test-name)
 
 (test-equal "vector->chunk-type"
-  '(IHDR #(73 72 68 82) "Image header")
-  (vector->chunk-type #(73 72 68 82)))
+  '(IHDR #vu8(73 72 68 82) "Image header")
+  (vector->chunk-type #vu8(73 72 68 82)))
 
 (test-equal "chunk-type->vector"
-  #(73 72 68 82)
+  #vu8(73 72 68 82)
   (chunk-type->vector 'IHDR))
 
 (test-equal "png-chunk-type/name"
   'IHDR
   (png-chunk-type/name (make <png-chunk>
-                         #:type #(73 72 68 82))))
+                         #:type #vu8(73 72 68 82))))
 
 (test-equal "png-chunk-type/description"
   "Image header"
   (png-chunk-type/description (make <png-chunk>
-                                #:type #(73 72 68 82))))
+                                #:type #vu8(73 72 68 82))))
 
 
 ;; IHDR
 
-(define %ihdr-data #(0 0 13 180 0 0 9 176 8 2 0 0 0))
+(define %ihdr-data #vu8(0 0 13 180 0 0 9 176 8 2 0 0 0))
 
 (test-equal "data->png-chunk:IHDR: width"
   3508
   (png-chunk:IHDR-width
    (data->png-chunk:IHDR %ihdr-data
-                         #(73 72 68 82)
+                         #vu8(73 72 68 82)
                          13
-                         #(119 50 219 167))))
+                         #vu8(119 50 219 167))))
 
 (test-equal "data->png-chunk:IHDR: height"
   2480
   (png-chunk:IHDR-height
    (data->png-chunk:IHDR %ihdr-data
-                         #(73 72 68 82)
+                         #vu8(73 72 68 82)
                          13
-                         #(119 50 219 167))))
+                         #vu8(119 50 219 167))))
 
 (test-equal "data->png-chunk:IHDR: bit-depth"
   8
   (png-chunk:IHDR-bit-depth
    (data->png-chunk:IHDR %ihdr-data
-                         #(73 72 68 82)
+                         #vu8(73 72 68 82)
                          13
-                         #(119 50 219 167))))
+                         #vu8(119 50 219 167))))
 
 
 (define exit-status (test-runner-fail-count (test-runner-current)))
