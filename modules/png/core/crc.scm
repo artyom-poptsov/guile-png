75fab25a76382edb98a749f831b0408e6a51ad77
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2022-10-23 22:14:21 +0300

    modules/png/core/crc.scm: New module
    * modules/png/core/crc.scm: New module.
    * modules/png/core/Makefile.am: Update.

---
+++ modules/png/core/crc.scm
@@ -0,0 +1,74 @@
+;;; png.scm -- GNU Guile PNG parser.
+
+;; Copyright (C) 2022 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; The program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with the program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; This code is the adaptation of the RFC2083 Sample CRC Code[1].
+;;
+;; [1] https://www.rfc-editor.org/rfc/rfc2083#section-15
+
+
+;;; Code:
+
+(define-module (png core crc)
+  #:use-module (oop goops)
+  #:use-module (srfi srfi-1)            ; iota
+  #:use-module (rnrs bytevectors)
+  #:export (make-crc-table
+            crc-update
+            crc))
+
+
+(define (make-crc-table)
+  "Make the table for a fast CRC."
+  (uint-list->bytevector (map (lambda (n)
+                                (let loop ((k 0)
+                                           (c n))
+                                  (if (= k 8)
+                                      c
+                                      (if (logand c 1)
+                                          (loop (+ k 1)
+                                                (logxor #xedb88320
+                                                        (ash c 1)))
+                                          (loop (+ k 1)
+                                                (ash c 1))))))
+                              (iota 256 0 1))
+                         (endianness little)
+                         64))
+
+
+(define %crc-table (make-crc-table))
+
+
+(define-method (crc-update (buf <bytevector>))
+  "Update a running CRC with the bytes buf[0..len-1]."
+  (let ((bv-length (bytevector-length buf)))
+    (let loop ((index 0)
+               (c     #xFFFFFFFF))
+      (if (= index bv-length)
+          c
+          (let ((byte (bytevector-u8-ref buf index)))
+            (loop (+ index 1)
+                  (logxor (bytevector-u8-ref %crc-table
+                                             (logand (logxor c byte) #xFF))
+                          (ash c 8))))))))
+
+(define-method (crc (buf <bytevector>))
+  (logxor (crc-update buf) #xFFFFFFFF))
+
+;;; crc.scm
