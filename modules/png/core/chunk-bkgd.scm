74295a0cd224f85fb359ed6bb1bee4492c0bb039
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2022-10-23 19:18:46 +0300

    png/core/chunk-bkgd: Fix a typo: "colour" -> "color"
    * modules/png/core/chunk-bkgd.scm: Fix a typo: "colour" -> "color".

--- modules/png/core/chunk-bkgd.scm
+++ modules/png/core/chunk-bkgd.scm
@@ -33,7 +33,7 @@
   #:use-module (png core common)
   #:use-module (png core chunk)
   #:export (<png-chunk:bKGD>
-            png-chunk:bKGD-colour-type
+            png-chunk:bKGD-color-type
             png-chunk:bKGD-grayscale
             png-chunk:bKGD-red
             png-chunk:bKGD-green
@@ -45,10 +45,10 @@
 
 (define-class <png-chunk:bKGD> (<png-chunk>)
   ;; <number>
-  (colour-type
-   #:init-keyword #:colour-type
+  (color-type
+   #:init-keyword #:color-type
    #:init-value   0
-   #:getter       png-chunk:bKGD-colour-type)
+   #:getter       png-chunk:bKGD-color-type)
 
   ;; <number>
   (grayscale
@@ -86,8 +86,8 @@
 
 (define-method (%display (chunk <png-chunk:bKGD>) (port <port>))
   (let ((type (vector->chunk-type (png-chunk-type chunk))))
-    (format port "#<png-chunk:bKGD colour type: ~a ~a>"
-            (png-chunk:bKGD-colour-type chunk)
+    (format port "#<png-chunk:bKGD color type: ~a ~a>"
+            (png-chunk:bKGD-color-type chunk)
             (object-address/hex-string chunk))))
 
 (define-method (display (chunk <png-chunk:bKGD>) (port <port>))
@@ -102,16 +102,16 @@
                                      (type        <vector>)
                                      (length      <number>)
                                      (crc         <vector>)
-                                     (colour-type <number>))
+                                     (color-type <number>))
   "Convert a PNG chunk data to a bKGD chunk instance."
-  (case colour-type
+  (case color-type
     ((0 4)
      (make <png-chunk:bKGD>
        #:length             length
        #:type               type
        #:data               data
        #:crc                crc
-       #:colour-type        colour-type
+       #:color-type        color-type
        #:greyscale          (vector->int16 data)))
     ((2 6)
      (make <png-chunk:bKGD>
@@ -119,7 +119,7 @@
        #:type               type
        #:data               data
        #:crc                crc
-       #:colour-type        colour-type
+       #:color-type        color-type
        #:red                (vector->int16 (vector-copy data 0 2))
        #:green              (vector->int16 (vector-copy data 2 4))
        #:blue               (vector->int16 (vector-copy data 4 6))))
@@ -129,7 +129,7 @@
        #:type               type
        #:data               data
        #:crc                crc
-       #:colour-type        colour-type
+       #:color-type        color-type
        #:palette-index      (vector-ref data 0)))))
 
 
