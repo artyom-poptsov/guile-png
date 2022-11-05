69812d0ba8f16c3ee9a2a6ebb3b8e257fa7042e2
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2022-11-05 14:17:15 +0300

    image-processing (png-image-filter-solarize): New method
    * modules/png/image-processing.scm (png-image-filter-solarize): New method.

--- modules/png/image-processing.scm
+++ modules/png/image-processing.scm
@@ -2,7 +2,9 @@
   #:use-module (oop goops)
   #:use-module (rnrs bytevectors)
   #:use-module (png image)
-  #:export (png-image-filter-invert-colors))
+  #:use-module (png pixel)
+  #:export (png-image-filter-invert-colors
+            png-image-filter-solarize))
 
 
 (define-method (png-image-filter-invert-colors (image <png-image>))
@@ -23,5 +25,25 @@
                                        (bytevector-u8-ref data (+ offset index))))
                 (loop-over-pixel (+ index 1))))
             (loop (+ offset pixel-size)))))))
+
+(define-method (png-image-filter-solarize (image     <png-image>)
+                                          (threshold <number>))
+  (let ((image-clone (png-image-clone image))
+        (pixel-count (png-image-pixels image)))
+    (let loop ((index 0))
+      (if (= index pixel-count)
+          image-clone
+          (let* ((pixel (png-image-pixel-ref image index))
+                 (red   (bytevector-u8-ref pixel 0))
+                 (green (bytevector-u8-ref pixel 1))
+                 (blue  (bytevector-u8-ref pixel 2)))
+            (when (< red threshold)
+              (bytevector-u8-set! pixel 0 (- 255 red)))
+            (when (< green threshold)
+              (bytevector-u8-set! pixel 1 (- 255 green)))
+            (when (< blue threshold)
+              (bytevector-u8-set! pixel 2 (- 255 blue)))
+            (png-image-pixel-set! image-clone index pixel)
+            (loop (+ index 1)))))))
 
 ;;; filter.scm ends here.
