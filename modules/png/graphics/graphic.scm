b51f0d064feb5672337d6653919de3a7322ea924
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2022-11-13 20:03:18 +0300

    modules/png/graphics/graphic.scm: Update the module commentary

--- modules/png/graphics/graphic.scm
+++ modules/png/graphics/graphic.scm
@@ -1,3 +1,36 @@
+;;; graphic.scm -- Generic graphic object.
+
+;; Copyright (C) 2022 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; The program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with the program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; This module contains description of a generic graphic object that is the
+;; root of the graphical object hierarchy.
+;;
+;; The inspiration for the hierarchy layout itself is an article named
+;; "Applying Object-Oriented Design to Structured Graphics" by John
+;; M. Vlissides and Mark A. Linton from Stanford University. [1]
+;;
+;; References:
+;; 1. https://www.softwarepreservation.org/projects/c_plus_plus/library/interviews/graphic.pdf
+
+
+;;; Code:
+
 (define-module (png graphics graphic)
   #:use-module (rnrs bytevectors)
   #:use-module (png image)
@@ -15,4 +48,4 @@
 (define-method (draw! (image <png-image>) (obj <graphic>))
   (error "Not implemented yet" image obj))
 
-;;; shape.scm ends here.
+;;; graphic.scm ends here.
