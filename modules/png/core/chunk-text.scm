229f1d547a7bb746bdf987469856e25375186bc6
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2022-11-16 00:09:19 +0300

    modules/png/core/chunk-text: New module
    * modules/png/core/chunk-text.scm: New module.
    * modules/png/core/Makefile.am: Update.

---
+++ modules/png/core/chunk-text.scm
@@ -0,0 +1,65 @@
+(define-module (png core chunk-text)
+  #:use-module (srfi srfi-43)
+  #:use-module (rnrs bytevectors)
+  #:use-module (oop goops)
+  #:use-module (png core common)
+  #:use-module (png core chunk)
+  #:export (<png-chunk:tEXt>
+            png-chunk:tEXt-keyword
+            png-chunk:tEXt-text
+            data->png-chunk:tEXt))
+
+
+
+(define-class <png-chunk:tEXt> (<png-chunk>)
+  ;; <string>
+  (keyword
+   #:init-value ""
+   #:getter     png-chunk:tEXt-keyword)
+
+  ;; <string>
+  (text
+   #:init-value ""
+   #:getter     png-chunk:tEXt-text))
+
+
+
+(define-method (%display (chunk <png-chunk:tEXt>) (port <port>))
+  (let ((type (png-chunk-type-info chunk)))
+    (format port "#<png-chunk:tEXt ~a: ~a ~a>"
+            (list-ref type 2)
+            (png-chunk:tEXt-keyword chunk)
+            (object-address/hex-string chunk))))
+
+
+
+(define-method (data->png-chunk:tEXt (data   <bytevector>)
+                                     (type   <symbol>)
+                                     (length <number>)
+                                     (crc    <number>))
+  (define (read-text index keyword)
+    (let loop ((text '())
+               (idx  index))
+      (if (= idx (bytevector-length data))
+          (make <png-chunk:tEXt>
+            #:length  length
+            #:type    type
+            #:data    data
+            #:crc     crc
+            #:keyword            keyword
+            #:text               (utf->string (reverse text)))
+          (loop (cons (bytevector-u8-ref data idx) text)
+                (+ idx 1)))))
+
+  (define (read-keyword)
+    (let loop ((keyword '())
+               (index   0))
+      (let ((byte (bytevector-u8-ref data index)))
+      (if (zero? byte)
+          (read-text index (utf8->string (reverse keyword)))
+          (loop (cons byte keyword)
+                (+ index 1))))))
+
+  (read-keyword))
+
+;;; chunk-text.scm ends here.
