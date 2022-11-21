3c87128171c182d53ac5f623b4d975dbaa8a3efc
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2022-11-21 11:48:18 +0300

    modules/png/core/common.scm: Add the module commentary

--- modules/png/core/common.scm
+++ modules/png/core/common.scm
@@ -1,3 +1,29 @@
+;;; common.scm -- Common Guile-PNG procedures.
+
+;; Copyright (C) 2022 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; The program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with the program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; This module contains common procedures that are used in different parts of
+;; Guile-PNG.
+
+
+;;; Code:
+
 (define-module (png core common)
   #:use-module (oop goops)
   #:use-module (rnrs bytevectors)
@@ -54,3 +80,5 @@ bytevectors."
                                             (calculate-length index))
                       result))
           (reverse result)))))
+
+;;; common.scm ends here.
