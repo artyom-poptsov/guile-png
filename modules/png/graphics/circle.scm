de6b93ba52d227a9840cacb58c9793b25f391fe5
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2022-12-05 13:38:59 +0300

    modules/png/graphics/circle.scm: Add the module commentary

--- modules/png/graphics/circle.scm
+++ modules/png/graphics/circle.scm
@@ -1,3 +1,28 @@
+;;; circle.scm -- Circle drawing algorithm implementation.
+
+;; Copyright (C) 2022 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; The program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with the program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; This module contains the implementation of the circle drawing algorithm.
+
+
+;;; Code:
+
 (define-module (png graphics circle)
   #:use-module (oop goops)
   #:use-module (png image)
