e22d762c5e522dd98994e00c0057e73a517ed1d9
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2022-12-08 20:48:37 +0300

    png/graphics/line (draw!): Bugfix
    * modules/png/graphics/line.scm (draw!): Re-implement the better version of
      Bresenham's line drawing algorithm based on the GNU Octave code from
      <https://www.markwrobel.dk/post/amiga-machine-code-letter12-linedraw/>

--- modules/png/graphics/line.scm
+++ modules/png/graphics/line.scm
@@ -20,44 +20,156 @@
 
 
 
+;; Based on the code from
+;; <https://www.markwrobel.dk/post/amiga-machine-code-letter12-linedraw/>
 (define-method (draw! (image <png-image>) (line <line>))
   "This method implements Bresenham's Line generation algorithm."
   (let* ((p1 (line-p1 line))
          (p2 (line-p2 line))
-         (x1 (if (< (point-x p1) (point-x p2))
-                 (point-x p1)
-                 (point-x p2)))
-         (y1 (if (< (point-y p1) (point-y p2))
-                 (point-y p1)
-                 (point-y p2)))
-         (x2 (if (> (point-x p2) (point-x p1))
-                 (point-x p2)
-                 (point-x p1)))
-         (y2 (if (> (point-y p2) (point-y p1))
-                 (point-y p2)
-                 (point-y p1)))
+         (x1 (point-x p1))
+         (y1 (point-y p1))
+         (x2 (point-x p2))
+         (y2 (point-y p2))
+         (dx (- x2 x1))
+         (dy (- y2 y1))
+         (a  (/ dy dx))
          (color (graphic-color line)))
-    (if (= x1 x2)
-        (let ((delta (if (> y2 y1)
-                         1
-                         -1)))
-          (let loop ((y y1))
-            (unless (= y y2)
-              (png-image-pixel-set! image x1 y color)
-              (loop (+ y delta)))))
-        (let ((m-new (* 2 (- y2 y1))))
-          (let loop ((x x1)
-                     (y y1)
-                     (slope-error (- m-new (- x2 x1))))
-            (when (<= x x2)
-              (png-image-pixel-set! image x y color)
-              (let ((slope-error-new (+ slope-error m-new)))
-                (if (>= slope-error-new 0)
-                    (loop (+ x 1)
-                          (+ y 1)
-                          (- slope-error-new (* 2 (- x2 x1))))
-                    (loop (+ x 1)
-                          y
-                          slope-error-new)))))))))
+    (png-image-pixel-set! image x1 y1 color)
+    (if (<= (abs dy) (abs dx))
+        (if (< dx 0)
+            (if (< dy 0)
+                ;; Octant 3
+                (let loop ((fraction (+ 0.5 a))
+                           (x x1)
+                           (y y1))
+                  (when (> x x2)
+                    (if (< fraction 1)
+                        (begin
+                          (png-image-pixel-set! image x y color)
+                          (loop (+ fraction a)
+                                (- x 1)
+                                y))
+                        (begin
+                          (png-image-pixel-set! image x y color)
+                          (loop (+ fraction a (- 1))
+                                (- x 1)
+                                (- y 1))))))
+                ;; Octant 4
+                (let loop ((fraction (+ 0.5 (- a)))
+                           (x x1)
+                           (y y1))
+                  (when (> x x2)
+                    (if (< fraction 1)
+                        (begin
+                          (png-image-pixel-set! image x y color)
+                          (loop (+ fraction (- a))
+                                (- x 1)
+                                y))
+                        (begin
+                          (png-image-pixel-set! image x y color)
+                          (loop (+ fraction (- a) (- 1))
+                                (- x 1)
+                                (+ y 1)))))))
+            (if (< dy 0)
+                ;; Octant 0
+                (let loop ((fraction (+ 0.5 (- a)))
+                           (x x1)
+                           (y y1))
+                  (when (< x x2)
+                    (if (< fraction 1)
+                        (begin
+                          (png-image-pixel-set! image x y color)
+                          (loop (+ fraction (- a))
+                                (+ x 1)
+                                y))
+                        (begin
+                          (png-image-pixel-set! image x y color)
+                          (loop (+ fraction (- a) (- 1))
+                                (+ x 1)
+                                (- y 1))))))
+                ;; Octant 7
+                (let loop ((fraction (+ 0.5 a))
+                           (x x1)
+                           (y y1))
+                  (when (< x x2)
+                    (if (< fraction 1)
+                        (begin
+                          (png-image-pixel-set! image x y color)
+                          (loop (+ fraction a)
+                                (+ x 1)
+                                y))
+                        (begin
+                          (png-image-pixel-set! image x y color)
+                          (loop (+ fraction a (- 1))
+                                (+ x 1)
+                                (+ y 1))))))))
+        ;; (> (abs dy) (abs dx))
+        (if (< dx 0)
+            (if (< dy 0)
+                ;; Octant 2
+                (let loop ((fraction (+ 0.5 (/ 1 a)))
+                           (x x1)
+                           (y y1))
+                  (when (> y y2)
+                    (if (< fraction 1)
+                        (begin
+                          (png-image-pixel-set! image x y color)
+                          (loop (+ fraction (/ 1 a))
+                                x
+                                (- y 1)))
+                        (begin
+                          (png-image-pixel-set! image x y color)
+                          (loop (+ fraction a (/ 1 a) (- 1))
+                                (- x 1)
+                                (- y 1))))))
+                ;; Octant 5
+                (let loop ((fraction (+ 0.5 (/ (- 1) a)))
+                           (x x1)
+                           (y y1))
+                  (when (< y y2)
+                    (if (< fraction 1)
+                        (begin
+                          (png-image-pixel-set! image x y color)
+                          (loop (+ fraction (- (-1) a))
+                                x
+                                (+ y 1)))
+                        (begin
+                          (png-image-pixel-set! image x y color)
+                          (loop (+ fraction (/ (- 1) a) (- 1))
+                                (- x 1)
+                                (+ y 1)))))))
+            (if (< dy 0)
+                ;; Octant 1
+                (let loop ((fraction (+ 0.5 (/ (- 1) a)))
+                           (x x1)
+                           (y y1))
+                  (when (> y y2)
+                    (if (< fraction 1)
+                        (begin
+                          (png-image-pixel-set! image x y color)
+                          (loop (+ fraction (/ (- 1) a))
+                                x
+                                (- y 1)))
+                        (begin
+                          (png-image-pixel-set! image x y color)
+                          (loop (+ fraction (/ (- 1) a) (- 1))
+                                (+ x 1)
+                                (- y 1))))))
+                ;; Octant 6
+                (let loop ((fraction (+ 0.5 (/ 1 a)))
+                           (x x1)
+                           (y y1))
+                  (when (< y y2)
+                    (if (< fraction 1)
+                        (begin
+                          (png-image-pixel-set! image x y color)
+                          (loop (+ fraction (/ 1 a))
+                                x
+                                (+ y 1)))
+                        (begin
+                          (png-image-pixel-set! image x y color)
+                          (loop (+ fraction (/ 1 a) (- 1))
+                                (+ x 1)
+                                (+ y 1)))))))))))
 
 ;;; line.scm ends here.
