f8bd6c80588865d924f4558240ea0e056dfeb142
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2022-12-11 22:45:44 +0300

    png/graphics/filled-rectangle: Add the module commentary
    * modules/png/graphics/filled-rectangle.scm: Add the module commentary.

--- modules/png/graphics/filled-rectangle.scm
+++ modules/png/graphics/filled-rectangle.scm
@@ -1,3 +1,28 @@
+;;; filled-rectangle.scm -- Filled rectangle implementation.
+
+;; Copyright (C) 2022 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; The program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with the program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; This module contains the implementation of filled rectangle.
+
+
+;;; Code:
+
 (define-module (png graphics filled-rectangle)
   #:use-module (oop goops)
   #:use-module (png image)
