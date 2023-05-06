cbfd609c5e790bdee02470b8619be777874202a1
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2023-05-06 09:53:32 +0300

    modules/png/chunk-decoder.scm: Add module commentary

--- modules/png/chunk-decoder.scm
+++ modules/png/chunk-decoder.scm
@@ -1,3 +1,28 @@
+;;; chunk-decoder.scm -- PNG chunk decoder.
+
+;; Copyright (C) 2022-2023 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; The program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with the program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; This module chunk decoder procedures.
+
+
+;;; Code:
+
 (define-module (png chunk-decoder)
   #:use-module (oop goops)
   #:use-module (png core chunk)
@@ -47,3 +72,5 @@
               (converter image chunk)
               chunk))
         (error "Unknown chunk type" type chunk))))
+
+;;; chunk-decoder.scm ends here.
