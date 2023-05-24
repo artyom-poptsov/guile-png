619adc05daf7c9003c23356df01db0f45590ac33
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2023-05-24 20:33:40 +0300

    png/core/chunk/chrm: Add module commentary
    * modules/png/core/chunk/chrm.scm: Add module commentary.

--- modules/png/core/chunk/chrm.scm
+++ modules/png/core/chunk/chrm.scm
@@ -1,3 +1,30 @@
+;;; chrm.scm -- bKGD chunk.
+
+;; Copyright (C) 2023 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; The program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with the program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; This module contains cHRM (Primary chromaticities and white point) chunk.
+;;
+;; <https://www.rfc-editor.org/rfc/rfc2083#page-20>
+
+
+;;; Code:
+
 (define-module (png core chunk chrm)
   #:use-module (srfi srfi-43)
   #:use-module (rnrs bytevectors)
