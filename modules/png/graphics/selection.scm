295a08a098d8c885a107925fdbe28a91715f0474
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2023-08-19 18:04:54 +0300

    modules/png/graphics/selection.scm: Add module commentary

--- modules/png/graphics/selection.scm
+++ modules/png/graphics/selection.scm
@@ -1,3 +1,29 @@
+;;; selection.scm -- Selected part of an image.
+
+;; Copyright (C) 2023 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; The program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with the program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; This module contains definition of the <selection> class that represents a
+;; selected part of an image.
+
+
+;;; Code:
+
 (define-module (png graphics selection)
   #:use-module (oop goops)
   #:use-module (png core common)
