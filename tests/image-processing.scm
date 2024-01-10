527d7d4ac2252fbd9166056788c44d524f35843d
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2024-01-10 22:35:33 +0300

    tests/image-processing.scm ("blur"): New test
    * tests/image-processing.scm ("blur"): New test.

--- tests/image-processing.scm
+++ tests/image-processing.scm
@@ -1,3 +1,21 @@
+;;; image-processing.scm -- Image processing tests.
+
+;; Copyright (C) 2023-2024 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; The program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with the program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 (use-modules (srfi srfi-64)
              (srfi srfi-26)
              (rnrs bytevectors)
@@ -7,8 +25,10 @@
              (png image)
              (png image-processing))
 
+
 (define %topdir (getenv "abs_top_srcdir"))
 (define %meditate (format #f "~a/tests/meditate.png" %topdir))
+(define %example-ellipse (format #f "~a/tests/example-ellipse.png" %topdir))
 
 (define %test-name "image-processing")
 
@@ -32,6 +52,10 @@
 (test-assert "solarize"
   (let ((image (png->scm (open-input-file %meditate))))
     (png-image-solarize image 5)))
+
+(test-assert "blur"
+  (let ((image (png->scm (open-input-file %example-ellipse))))
+    (png-image-blur image)))
 
 
 (define exit-status (test-runner-fail-count (test-runner-current)))
