c7bb3001541b60612c66efb1ba6ffcbe16645054
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2025-04-07 23:48:08 +0300

    png/graphics/hexagon (initialize): Bugfix
    * modules/png/graphics/hexagon.scm (initialize): Bugfix: Set the hexagon
    diameter slot.

--- modules/png/graphics/hexagon.scm
+++ modules/png/graphics/hexagon.scm
@@ -101,6 +101,7 @@
                                      #:y (int (+ (* radius (sin angle))
                                                  cy)))
                                    points))))))
+    (slot-set! hexagon 'diameter diameter)
     (slot-set! hexagon 'points points)))
 
 ;;; hexagon.scm ends here.
