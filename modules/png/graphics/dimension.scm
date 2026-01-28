de914ee8f9243f704227418e2c947c4dfe4516af
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2026-01-28 21:55:50 +0300

    png/graphics/{dimension,point}: Add comments
    * modules/png/graphics/dimension.scm, modules/png/graphics/point.scm: Add the
    module commentary.  Add comments and docstrings.

--- modules/png/graphics/dimension.scm
+++ modules/png/graphics/dimension.scm
@@ -1,3 +1,29 @@
+;;; dimension.scm -- Dimension class implementation.
+
+;; Copyright (C) 2023-2026 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; The program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with the program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; This module contains the implementation of the <dimension> class that
+;; describes a dimension of a 2D object.
+
+
+;;; Code:
+
 (define-module (png graphics dimension)
   #:use-module (oop goops)
   #:use-module (png core common)
@@ -8,13 +34,18 @@
 
 
 
+;; A dimension of a 2D object.
 (define-class <dimension> ()
+  ;; Width.
+  ;;
   ;; <number>
   (width
    #:init-value   0
    #:init-keyword #:width
    #:getter       dimension-width)
 
+  ;; Height.
+  ;;
   ;; <number>
   (height
    #:init-value   0
--- modules/png/graphics/point.scm
+++ modules/png/graphics/point.scm
@@ -1,3 +1,29 @@
+;;; point.scm -- Dimension class implementation.
+
+;; Copyright (C) 2022-2026 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; The program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with the program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; This module contains the implementation of the <point> class that describes
+;; a point in 2D space.
+
+
+;;; Code:
+
 (define-module (png graphics point)
   #:use-module (oop goops)
   #:use-module (png image)
@@ -8,11 +34,20 @@
             point-x
             point-y))
 
+
+;; A point in 2D space.
 (define-class <point> (<graphic>)
+  ;; A coordinate on X axis.
+  ;;
+  ;; <number>
   (x
    #:init-value   0
    #:init-keyword #:x
    #:getter       point-x)
+
+  ;; A coordinate on Y axis.
+  ;;
+  ;; <number>
   (y
    #:init-value   0
    #:init-keyword #:y
@@ -34,6 +69,7 @@
 
 
 (define-method (draw! (image <png-image>) (point <point>))
+  "Draw a @var{point} (a pixel) on an @var{image} instance."
   (png-image-pixel-set! image
                         (point-x point)
                         (point-y point)
